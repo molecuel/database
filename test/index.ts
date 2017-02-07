d019afa0db89db6ab3223f819c12456c32184643
dominicboettger <dominic.boettger@inspirationlabs.com>
2017-02-07 09:47:37 +0100

    Added unit test skeletons

--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,32 @@
 'use strict';
 import 'reflect-metadata';
-import should = require('should');
-import assert = require('assert');
+import * as assert from 'assert';
 import {di, injectable} from '@molecuel/di';
 import {Subject, Observable} from '@reactivex/rxjs';
-should();
 
-describe('<%= name %>', function() {
+describe('MlclDatabase', function() {
+  it('should be possible to load the database config', () => {
 
+  });
+  it('should be possible to init all configured Database interfaces', () => {
+
+  });
+  it('should be possible to access the database connections', () => {
+
+  });
+  it('should be possible to store data in persistence layer', () => {
+
+  });
+  it('should be possible to read data from the persistence layer', () => {
+
+  });
+  it('should be possible to populate data', () => {
+
+  });
+  it('should be possible to store and populate data in the population layer', () => {
+
+  });
+  it('should be possible to read data from the population layer', () => {
+
+  });
 }); // test end
