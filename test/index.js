28f25ccd8e3a2df4acf2bdb20798b7b5714e4424
dominicboettger <dominic.boettger@inspirationlabs.com>
2017-02-07 09:52:41 +0100

    tslint

--- test/index.js
+++ test/index.js
@@ -1,6 +1,20 @@
 'use strict';
 require("reflect-metadata");
-const should = require("should");
-should();
-describe('<%= name %>', function () {
+describe('MlclDatabase', function () {
+    it('should be possible to load the database config', () => {
+    });
+    it('should be possible to init all configured Database interfaces', () => {
+    });
+    it('should be possible to access the database connections', () => {
+    });
+    it('should be possible to store data in persistence layer', () => {
+    });
+    it('should be possible to read data from the persistence layer', () => {
+    });
+    it('should be possible to populate data', () => {
+    });
+    it('should be possible to store and populate data in the population layer', () => {
+    });
+    it('should be possible to read data from the population layer', () => {
+    });
 });
