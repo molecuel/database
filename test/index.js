c2d14c1720e4af4398fbad861160d119f13d1a48
Dominic Böttger <Dominic.Boettger@inspirationlabs.com>
2017-03-13 18:23:27 +0100

    fixed version

--- test/index.js
+++ test/index.js
@@ -13,6 +13,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const should = require("should");
 const assert = require("assert");
